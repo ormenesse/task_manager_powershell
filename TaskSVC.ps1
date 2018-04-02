8043cce46842594ad64bd66a7e44d09011001e4e
Vinícius Ormenesse <viniciusormenesse@gmail.com>
2018-04-02 15:00:49 -0300

    Update TaskSVC.ps1

--- TaskSVC.ps1
+++ TaskSVC.ps1
@@ -1,43 +1,32 @@
-echo "####################################" > CylanceSvc.csv
-echo "#       Análise  CylanceSvc        #" >> CylanceSvc.csv
-echo "####################################" >> CylanceSvc.csv
-echo "####################################" > CyOptics.csv
-echo "#        Análise  CyOptics         #" >> CyOptics.csv
-echo "####################################" >> CyOptics.csv
-echo "####################################" > Maquina.csv
-echo "#         Análise  Máquina         #" >> Maquina.csv
-echo "####################################" >> Maquina.csv
-Get-WmiObject -Class Win32_PerfFormattedData_PerfProc_Process -Filter "name='CylanceSvc'" | findstr.exe IDProcess >> CylanceSvc.csv
-echo "Hora(min);IO(MBps);RAM(MB);CPU(%)" >> CylanceSvc.csv
-Get-WmiObject -Class Win32_PerfFormattedData_PerfProc_Process -Filter "name='CylanceOptics'" | findstr.exe IDProcess >> CyOptics.csv
-echo "Hora(min);IO(MBps);RAM(MB);CPU(%)" >> CyOptics.csv
-echo "Hora(min);IO(MBps);RAM(MB);CPU(%)" >> Maquina.csv
-
+for ($i=0; $i -lt $args.length; $i++)
+{
+    $servico = "name='$($args[$i])'"
+    Get-WmiObject -Class Win32_PerfFormattedData_PerfProc_Process -Filter $servico | findstr.exe IDProcess > "$($args[$i]).csv"
+    echo "Hora(min);IO(MBps);RAM(MB);CPU(%)" > "$($args[$i]).csv"
+}
+echo "Hora(min);IO(MBps);RAM(MB);CPU(%)" > Maquina.csv
 do
 {
-	#CylanceSvc.exe
-	$minutos =  $((new-timespan -hour (get-date).hour).totalminutes + (get-date).minute)
-	$ProcessProp = Get-WmiObject -Class Win32_PerfFormattedData_PerfProc_Process -Filter "name='CylanceSvc'"
-	$io = $ProcessProp.IODataBytesPersec/1024
-	$io = [Math]::Round(($ProcessProp.IODataBytesPersec / 1mb),2)
-	$ram = [Math]::Round(($ProcessProp.workingSetPrivate / 1mb),2)
-	$cpu = $ProcessProp.PercentProcessorTime
-	Write-Output "$($minutos);$($io);$($ram);$($cpu)" >> CylanceSvc.csv
-	#CyOptics.exe
-	$minutos =  $((new-timespan -hour (get-date).hour).totalminutes + (get-date).minute)
-	$ProcessProp = Get-WmiObject -Class Win32_PerfFormattedData_PerfProc_Process -Filter "name='CyOptics'"
-	$io = $ProcessProp.IODataBytesPersec/1024
-	$io = [Math]::Round(($ProcessProp.IODataBytesPersec / 1mb),2)
-	$ram = [Math]::Round(($ProcessProp.workingSetPrivate / 1mb),2)
-	$cpu = $ProcessProp.PercentProcessorTime
-	Write-Output "$($minutos);$($io);$($ram);$($cpu)" >> CyOptics.csv
+    for ($i=0; $i -lt $args.length; $i++)
+    {
+        $servico = "name='$($args[$i])'"
+	    $minutos =  $((new-timespan -hour (get-date).hour).totalminutes + (get-date).minute)
+	    $ProcessProp = Get-WmiObject -Class Win32_PerfFormattedData_PerfProc_Process -Filter $servico
+	    $io = $ProcessProp.IODataBytesPersec/1024
+	    $io = [Math]::Round(($ProcessProp.IODataBytesPersec / 1mb),2)
+	    $ram = [Math]::Round(($ProcessProp.workingSetPrivate / 1mb),2)
+	    $cpu = $ProcessProp.PercentProcessorTime
+	    Write-Output "$($minutos);$($io);$($ram);$($cpu)" >> "$($args[$i]).csv"
+    }
+
 	#Maquina
 	$minutos =  $((new-timespan -hour (get-date).hour).totalminutes + (get-date).minute)
 	$ProcessProp = Get-WmiObject -Class Win32_PerfFormattedData_PerfProc_Process -Filter "name='_Total'"
+	$ProcessIdle = Get-WmiObject -Class Win32_PerfFormattedData_PerfProc_Process -Filter "name='Idle'"
 	$io = $ProcessProp.IODataBytesPersec/1024
 	$io = [Math]::Round(($ProcessProp.IODataBytesPersec / 1mb),2)
 	$ram = [Math]::Round(($ProcessProp.workingSetPrivate / 1mb),2)
-	$cpu = $ProcessProp.PercentProcessorTime
+	$cpu = ($ProcessProp.PercentProcessorTime - $ProcessIdle.PercentProcessorTime)
 	Write-Output "$($minutos);$($io);$($ram);$($cpu)" >> Maquina.csv
 	start-sleep -Seconds 120
 }
