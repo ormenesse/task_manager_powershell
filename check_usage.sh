68647675192c032b1ddac0babf5f21415fe6dc36
Vinícius Ormenesse <viniciusormenesse@gmail.com>
2018-09-21 19:00:48 -0300

    Update check_usage.sh

--- check_usage.sh
+++ check_usage.sh
@@ -6,16 +6,15 @@ cores=$(nproc)
 clear
 while true
 do
-  tput cuu1
-  echo ""
+  tput cup 0 0
+  tput el
+  tput el1
   echo -e "USER\t\t%CPU\t\t%MEM"
-  
   # main loop
-
   for user in `ps -ef | grep -v UID | awk '{print $1}'| sort | uniq | awk '/^[^0-9,+]+[^\+]$/ {print $1}'`;
   do
     top -b -n 1 -u $user | awk -v var="$user" -v core=$cores 'NR>7 { sumC += $9; }; { sumM += $10; } END { print var "\t\t" sumC/core "\t\t" sumM; }';
-  done | sort -n `echo $var`
-  sleep 10
+  done | sort -n `echo $var` | tee /dev/tty | awk -v MM=$pmem 'NR>0 { sumCPU += $2}; { sumMem += $3} END { print "TOTAL\t\t" sumCPU "\t\t" MM*100; }'
+  sleep 5
   tput cup 0
 done
