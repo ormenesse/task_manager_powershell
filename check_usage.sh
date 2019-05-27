7107ba897ffec823bcf0f6683ab9740a2fb987f5
Vinícius Ormenesse <viniciusormenesse@gmail.com>
2019-05-27 14:21:47 -0300

    Update check_usage.sh

--- check_usage.sh
+++ check_usage.sh
@@ -2,10 +2,17 @@
 #
 # script checking CPU and memory utilization by user
 #
+echo "Check Server Usage by User"
 cores=$(nproc)
+pmem=$(free | awk 'NR==2 {print 1-$7/$2;};')
+n=$((0))
 clear
 while true
 do
+  n=$((n+1))
+  if [ $n = 100]; then
+    tput Ed
+  fi
   tput cup 0 0
   tput el
   tput el1
@@ -16,5 +23,5 @@ do
     top -b -n 1 -u $user | awk -v var="$user" -v core=$cores 'NR>7 { sumC += $9; }; { sumM += $10; } END { print var "\t\t" sumC/core "\t\t" sumM; }';
   done | sort -n `echo $var` | tee /dev/tty | awk -v MM=$pmem 'NR>0 { sumCPU += $2}; { sumMem += $3} END { print "TOTAL\t\t" sumCPU "\t\t" MM*100; }'
   sleep 5
-  tput cup 0
+  tput cup 0 && tput cup 0
 done
